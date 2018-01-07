4178e5594b0320ea6efc4cb1706ae55686dea7a7
b1f6c1c4 <b1f6c1c4@gmail.com>
2018-01-08 03:23:28 +0800

    Mongodb on kubernetes

---
+++ k8s/apply.ps1
@@ -0,0 +1,170 @@
+Function May-Quit($enabled)
+{
+  if (!$enabled)
+  {
+    return $False
+  }
+
+  Write-Host -NoNewline "Continue? q to quit: "
+  return $($(Read-Host) -eq "q")
+}
+
+Function kb-apply-t
+{
+  Param (
+    [Parameter(Position=0, Mandatory=$True)]
+    [string]$File,
+    [string]$Namespace = "production",
+    [int]$Instance = 1,
+    [int]$Size = 10,
+    [string]$Shard = 1,
+    [switch]$Delete = $False
+  )
+
+  if (!$Delete)
+  {
+    sed -e "s/NAMESPACE_ID/$Namespace/g; s/INSTANCE/$Instance/g; s/SIZE/$Size/g; s/hardX/hard${Shard}/g" $File | kubectl apply -f -
+  }
+  else
+  {
+    sed -e "s/NAMESPACE_ID/$Namespace/g; s/INSTANCE/$Instance/g; s/SIZE/$Size/g; s/hardX/hard${Shard}/g" $File | kubectl delete -f -
+  }
+}
+
+Function kb-mongo-up
+{
+  Param (
+    [string]$Namespace = "production",
+    [switch]$Confirm = $True,
+    [int]$Instances = 2,
+    [int]$SizeConfig = 5,
+    [int]$SizeMain = 10,
+    [string]$Shards = 2,
+    [switch]$Delete = $False
+  )
+
+  kb-apply-t namespace.yaml -Namespace $Namespace -Delete:$Delete
+  kubectl get ns
+  if (May-Quit $Confirm) { return; }
+
+  kb-apply-t gce-ssd-storageclass.yaml
+  kubectl get sc
+  if (May-Quit $Confirm) { return; }
+
+  if (!$Delete)
+  {
+    gcloud compute disks create --size "${SizeConfig}GB" --type pd-ssd "pd-ssd-disk-k8s-mongodb-${Namespace}-${SizeConfig}g-0"
+    For ($i = 1; $i -le $Instances; $i++)
+    {
+      gcloud compute disks create --size "${SizeMain}GB" --type pd-ssd "pd-ssd-disk-k8s-mongodb-${Namespace}-${SizeMain}g-${i}"
+    }
+  }
+  else
+  {
+    gcloud compute disks delete "pd-ssd-disk-k8s-mongodb-${Namespace}-${SizeConfig}g-0"
+    For ($i = 1; $i -le $Instances; $i++)
+    {
+      gcloud compute disks delete "pd-ssd-disk-k8s-mongodb-${Namespace}-${SizeMain}g-${i}"
+    }
+  }
+
+  kb-apply-t ext4-gce-ssd-persistentvolume.yaml -Namespace $Namespace -Size $SizeConfig -Instance 0 -Delete:$Delete
+  For ($i = 1; $i -le $Instances; $i++)
+  {
+    kb-apply-t ext4-gce-ssd-persistentvolume.yaml -Namespace $Namespace -Size $SizeMain -Instance $i -Delete:$Delete
+  }
+  kubectl get pv
+  if (May-Quit $Confirm) { return; }
+
+  kb-apply-t mongodb-configdb-service-stateful.yaml -Namespace $Namespace -Size $SizeConfig -Delete:$Delete
+  kubectl get svc,po "--namespace=$Namespace"
+  if (May-Quit $Confirm) { return; }
+
+  For ($i = 1; $i -le $Shards; $i++)
+  {
+    kb-apply-t mongodb-maindb-service-stateful.yaml -Namespace $Namespace -Size $SizeMain -Shard $i -Delete:$Delete
+  }
+  kubectl get svc,po "--namespace=$Namespace"
+  if (May-Quit $Confirm) { return; }
+
+  kb-apply-t mongodb-mongos-deployment-service.yaml -Namespace $Namespace -Delete:$Delete
+  kubectl get svc,po "--namespace=$Namespace"
+  if (May-Quit $Confirm) { return; }
+
+  if (!$Delete)
+  {
+    kb-mongo-config -Namespace $Namespace -Shards $Shard
+  }
+}
+
+Function kb-mongo-config
+{
+  Param (
+    [string]$Namespace = "production",
+    [string]$Shards = 2,
+    [switch]$Configsvr = $True,
+    [switch]$Maindb = $True,
+    [switch]$Mongos = $True
+  )
+
+  $tmp = ".mongo-init.js~"
+
+  if ($Configsvr)
+  {
+    $script = @"
+rs.initiate({
+  _id: "${Namespace}-configsvr",
+  members: [{
+  _id: 0,
+  host: "mongodb-configdb-0.mongodb-configdb-headless-service.${Namespace}.svc.cluster.local:27019",
+}]});
+"@
+    sc $tmp $script
+
+    echo "Exec rs.initiate on mongodb-configdb-0"
+    kubectl cp $tmp "$Namespace/mongodb-configdb-0:/tmp/mongo-init.js" -c "mongodb-configdb-container"
+    kubectl exec "--namespace=$Namespace" "mongodb-configdb-0" -c "mongodb-configdb-container" -- mongo --port 27019 /tmp/mongo-init.js
+  }
+
+  if ($Maindb)
+  {
+    For ($i = 1; $i -le $Shards; $i++)
+    {
+      $script = @"
+rs.initiate({
+  _id: "${Namespace}-shard${i}",
+  members: [{
+  _id: 0,
+  host: "mongodb-shard${i}-0.mongodb-shard${i}-headless-service.${Namespace}.svc.cluster.local:27017",
+}]});
+"@
+      sc $tmp $script
+
+      echo "Exec rs.initiate on mongodb-shard${i}-0"
+      kubectl cp $tmp "$Namespace/mongodb-shard${i}-0:/tmp/mongo-init.js" -c "mongodb-shard${i}-container"
+      kubectl exec "--namespace=$Namespace" "mongodb-shard${i}-0" -c "mongodb-shard${i}-container" -- mongo --port 27017 /tmp/mongo-init.js
+    }
+  }
+
+  if ($Mongos)
+  {
+    $script = ""
+    For ($i = 1; $i -le $Shards; $i++)
+    {
+      $script = $script + @"
+sh.addShard("mongodb-${Namespace}/mongodb-shard${i}-0.mongodb-shard${i}-headless-service.${Namespace}.svc.cluster.local:27017");
+
+"@
+    }
+    sc $tmp $script
+    $pods = @(kubectl get pod "--namespace=$Namespace" -l "tier=routers" -o 'jsonpath="{range .items[*]}{.metadata.name}{\"\n\"}{end}"')
+    ForEach ($pod In $pods)
+    {
+      echo "Exec sh.addShard on ${pod}"
+      kubectl cp $tmp "$Namespace/${pod}:/tmp/mongo-init.js" -c "mongos-container"
+      kubectl exec "--namespace=$Namespace" $pod -c "mongos-container" -- mongo --port 27017 /tmp/mongo-init.js
+    }
+  }
+
+  rm $tmp
+}
