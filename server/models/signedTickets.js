1c92a5d5ba70b2596575bc94ce1034201ee700a5
b1f6c1c4 <b1f6c1c4@gmail.com>
2018-01-20 15:44:04 +0800

    Mongoose

---
+++ server/models/signedTickets.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+
+const TicketSchema = new Schema({
+  bId: {
+    type: String,
+    lowercase: true,
+  },
+  result: {
+    type: [String],
+  },
+}, {
+  _id: false,
+  id: false,
+});
+
+const SignedTicketSchema = new Schema({
+  _id: {
+    type: String,
+    lowercase: true,
+  },
+  payload: TicketSchema,
+  s: {
+    type: [String],
+    lowercase: true,
+  },
+  c: {
+    type: [String],
+    lowercase: true,
+  },
+}, {
+  id: false,
+  shardKey: {
+    _id: 1,
+  },
+  timestamps: { },
+});
+
+module.exports = mongoose.model('signedTickets', SignedTicketSchema);
