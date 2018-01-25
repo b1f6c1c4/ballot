83f1b596c5444f9707e142d62077bf0c49d6f349
b1f6c1c4 <b1f6c1c4@gmail.com>
2018-01-25 13:15:23 +0800

    Merge files

--- server/tests/util.js
+++ server/tests/util.js
@@ -4,7 +4,6 @@ const { Organizer } = require('../models/organizers');
 const { Ballot } = require('../models/ballots');
 const { SubmittedTicket } = require('../models/submittedTickets');
 const { SignedTicket } = require('../models/signedTickets');
-const thrower = require('./mongooseThrower');
 const logger = require('../logger')('tests/util');
 
 const models = {
@@ -12,6 +11,61 @@ const models = {
   Ballot,
   SubmittedTicket,
   SignedTicket,
+};
+
+const thrower = (model) => {
+  logger.info('Hacking model', model.modelName);
+  let toThrow = {};
+  const verbs1 = [
+    'save',
+    'remove',
+  ];
+  const verbs2 = [
+    'count',
+    'find',
+    'findOne',
+    'findOneAndRemove',
+    'findOneAndUpdate',
+    'update',
+  ];
+  verbs1.forEach((verb) => {
+    logger.trace('Inject hacker for schema', verb);
+    // eslint-disable-next-line func-names
+    model.schema.s.hooks.pre(verb, function (next) {
+      logger.trace('Verb called, hack now', verb);
+      const obj = _.get(toThrow, verb);
+      if (obj !== undefined) {
+        logger.warn('Hacked by', obj);
+        logger.warn('fff', this);
+        expect(verb).toEqual(verb);
+      }
+      if (obj) {
+        next(obj);
+      } else {
+        next();
+      }
+    });
+  });
+  verbs2.forEach((verb) => {
+    logger.trace('Inject hacker for model', verb);
+    // eslint-disable-next-line func-names
+    model.hooks.pre(verb, function (next) {
+      logger.trace('Verb called, hack now', verb);
+      const obj = _.get(toThrow, verb);
+      if (obj !== undefined) {
+        logger.warn('Hacked by', obj);
+        expect(this.op).toEqual(verb);
+      }
+      if (obj) {
+        next(obj);
+      } else {
+        next();
+      }
+    });
+  });
+  return (th) => {
+    toThrow = th;
+  };
 };
 
 _.mapValues(models, (M) => {
